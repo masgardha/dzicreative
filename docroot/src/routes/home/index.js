3d8663cc06d280d314a10c9d1b918dd28a9dcbc1
Mas Gardha <masgardha@gmail.com>
2022-11-18 11:18:09 +0700

    selamat datang

--- docroot/src/routes/home/index.js
+++ docroot/src/routes/home/index.js
@@ -4,7 +4,7 @@ import style from './style.css';
 const Home = () => (
 	<div class={style.home}>
 		<h1>Home</h1>
-		<p>This is the Home component.</p>
+		<p>Cetak foto murah media mdf 6mm.</p>
 	</div>
 );
 
